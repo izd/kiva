45734f3405add559125c7107b9eae1affcd7ee6c
Isaac Elias <isaac@walkerandcobrands.com>
2015-03-21 20:26:36 -0700

    request is working, have to get the right data from the json response.

---
+++ app/src/main/java/com/zackhsi/kiva/KivaApplication.java
@@ -0,0 +1,21 @@
+package com.zackhsi.kiva;
+
+import android.content.Context;
+
+/*
+ * This is the Android application itself and is used to configure various settings
+ * including the image cache in memory and on disk. This also adds a singleton
+ * for accessing the relevant rest client.
+ *
+ *     TwitterClient client = TwitterApplication.getRestClient();
+ *     // use client to send requests to API
+ *
+ */
+public class KivaApplication{
+	private static Context context;
+
+
+//	public static KivaClient getRestClient() {
+//		return (KivaClient) KivaClient.getInstance(KivaClient.class, KivaApplication.context);
+//	}
+}
---
+++ app/src/main/java/com/zackhsi/kiva/models/Loan.java
@@ -0,0 +1,32 @@
+package com.zackhsi.kiva.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by isaac on 3/21/15.
+ */
+public class Loan {
+  int id;
+
+
+  public ArrayList<Loan> fromJson(JSONObject json){
+    ArrayList<Loan> result = new ArrayList<>();
+    try{
+      JSONArray loans = json.getJSONArray("loans");
+      for (int i = 0; i < loans.length() ; i++) {
+        JSONObject jsonLoan = loans.getJSONObject(i);
+        Loan loan = new Loan();
+        loan.id = jsonLoan.getInt("id");
+        result.add(loan);
+      }
+
+    }catch (JSONException e){
+      e.printStackTrace();
+    }
+    return result;
+  }
+}
